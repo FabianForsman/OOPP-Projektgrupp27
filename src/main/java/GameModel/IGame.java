3d521cafb7e6d06aa1b843e4177bff1837701a23
josefjakobson <j.jakobson28@gmail.com>
2022-09-13 15:22:30 +0200

    Interface for all games

---
+++ src/main/java/GameModel/IGame.java
@@ -0,0 +1,20 @@
+package GameModel;
+
+public interface IGame {
+    void setPlayerAmount();
+
+    void setCurrentPlayer();
+
+    void nextTurn();
+
+    void nextRound();
+
+    void quitGame();
+
+    void startGame();
+
+    void startRound();
+
+
+
+}
