36a03bfe481a46e035cd174072798fdaddf08d56
josefjakobson <j.jakobson28@gmail.com>
2022-09-13 16:22:22 +0200

    Interface, abstract class for all games. Start of HigherLower game class

---
+++ src/main/java/GameModel/Game.java
@@ -0,0 +1,35 @@
+/*
+package GameModel;
+
+import java.util.ArrayList;
+
+public abstract class Game {
+    private int playerAmount;
+    private ArrayList<Player> playerList = new ArrayList<Player>();
+    private String rules;
+    private Player currentPlayer;
+
+
+    private void setPlayerAmount(int amount) {
+        this.playerAmount = amount;
+    }
+
+    private void addPlayer(Player player){
+        this.playerList.add(player);
+    }
+
+    private void setCurrentPlayer() {
+
+    }
+    abstract void nextTurn();
+
+    abstract void nextRound();
+
+    abstract void quitGame();
+
+    abstract void startGame();
+
+    abstract void startRound();
+
+}
+*/
---
+++ src/main/java/GameModel/HigherLowerGame.java
@@ -0,0 +1,38 @@
+/*
+package GameModel;
+
+public class HigherLowerGame extends Game{
+    // private Deck deck = new Deck
+
+
+    public HigherLowerGame(){
+
+    }
+
+    public void nextTurn() {
+
+    }
+
+    @Override
+    public void nextRound() {
+
+    }
+
+    @Override
+    public void quitGame() {
+
+    }
+
+    @Override
+    public void startGame() {
+
+    }
+
+    @Override
+    public void startRound() {
+
+    }
+
+
+}
+*/
