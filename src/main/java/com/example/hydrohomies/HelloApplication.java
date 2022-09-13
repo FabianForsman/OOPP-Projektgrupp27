d87130aa4023f2f99129896d577ad31d42391f96
Fabian Forsman <fabian.forsman@gmail.com>
2022-09-13 11:24:31 +0200

    Created a Deck and its Cards.

--- src/main/java/com/example/hydrohomies/HelloApplication.java
+++ src/main/java/com/example/hydrohomies/HelloApplication.java
@@ -1,8 +1,7 @@
 package com.example.hydrohomies;
 
-import Model.CardGames.Cards.DeckOfCards;
-import Model.CardGames.Cards.IDeckOfCards;
-import Model.DiceGames.Dice.Dice;
+import Model.Cards.DeckOfCards;
+import Model.Cards.IDeckOfCards;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -22,29 +21,8 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         System.out.println("Starting Program.");
-        diceTests();
+        DeckOfCards deck = new DeckOfCards();
+        deck.ShowDeck();
         launch();
-    }
-
-    public static void deckTests(){
-        IDeckOfCards deck = new DeckOfCards();
-        deck.shuffle();
-        System.out.println("----------Shuffled deck----------");
-        deck.showDeck();
-        for(int i = 0; i < 40; i++)
-            deck.discardCard(deck.drawCard());
-        System.out.println("Removed 40 cards from the deck.");
-        deck.showDeck();
-        deck.returnDiscardPile();
-        System.out.println("Returned the discard pile to the deck.");
-        deck.showDeck();
-    }
-
-    public static void diceTests(){
-        Dice dice = new Dice(5); // Create 5 die
-        System.out.println(dice.getDiceValues());
-        dice.rollAllDice();
-        System.out.println(dice.getDiceValues());
-
     }
 }
