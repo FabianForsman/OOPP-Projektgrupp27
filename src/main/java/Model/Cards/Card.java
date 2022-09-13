3a5b5f94608ba13231c09cfd1f01c060ef79d599
Fabian Forsman <fabian.forsman@gmail.com>
2022-09-13 11:24:31 +0200

    Created a Deck and its Cards.

---
+++ src/main/java/Model/Cards/Card.java
@@ -0,0 +1,40 @@
+package Model.Cards;
+
+enum Suits {
+    SPADES,
+    HEARTS,
+    DIAMONDS,
+    CLUBS
+}
+
+enum Ranks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
+
+
+public class Card {
+
+    private final Suits suit;
+    private final Ranks rank;
+
+    public Card(Suits suit, Ranks rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public void DisplayCard() {
+        System.out.println(suit + " " + rank + " " + rank.ordinal());
+    }
+}
