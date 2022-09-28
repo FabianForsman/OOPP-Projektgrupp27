ed19650cf7c76de2039030af675abf92960e44f8
dpmram <93663582+dpmram@users.noreply.github.com>
2022-09-28 16:48:51 +0200

    Added more tests Updated passDiceLeft and PassDiceRight Added giveDice method Added setRandomCurrentPlayer method Added startDropTimer method Added checkOneOrSix method Added OpusKeyAdapter class

--- src/main/Tests/OpusTest.java
+++ src/main/Tests/OpusTest.java
@@ -1,30 +1,75 @@
+import Model.CardGames.Game.HigherLowerGame;
 import Model.DiceGames.Opus.OpusGamePanel;
 import Model.Player.Player;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class OpusTest {
-    ArrayList<Player> playerList = new ArrayList<>();
-    OpusGamePanel panel = new OpusGamePanel("test");
-
-    Player p1 = new Player("p1");
-    Player p2 = new Player("p2");
-    Player p3 = new Player("p3");
-
 
     @Test
-    public void testSetCurrentPlayer() {
-        playerList.add(p1);
-        playerList.add(p2);
-        playerList.add(p3);
-        panel.setCurrentPlayer(p2);
+    void addPlayerToPlayerList() {
+        OpusGamePanel testPanel= new OpusGamePanel("test");
+        Player player1 = new Player("player1");
+        testPanel.addPlayer(player1);
+        assertEquals(testPanel.getPlayerList().size(), 1);
+    }
 
+    @Test
+    void setCurrentPlayer() {
+        OpusGamePanel testPanel = new OpusGamePanel("test");
+        Player p1 = new Player("p1");
+        testPanel.addPlayer(p1);
+        testPanel.setCurrentPlayer(p1);
+        assertSame(p1, testPanel.getCurrentPlayer());
+    }
 
+    @Test
+    void testPassDiceLeft() {
+        OpusGamePanel testPanel = new OpusGamePanel("test");
+        Player p1 = new Player("p1");
+        Player p2 = new Player("p2");
+        testPanel.addPlayer(p1);
+        testPanel.addPlayer(p2);
+        testPanel.setCurrentPlayer(p1);
+        testPanel.passDiceLeft();
+        assertSame(p2, testPanel.getCurrentPlayer());
+    }
 
+    @Test
+    void testPassDiceRight() {
+        OpusGamePanel testPanel = new OpusGamePanel("test");
+        Player p1 = new Player("p1");
+        Player p2 = new Player("p2");
+        testPanel.addPlayer(p1);
+        testPanel.addPlayer(p2);
+        testPanel.setCurrentPlayer(p2);
+        testPanel.passDiceRight();
+        assertSame(p1, testPanel.getCurrentPlayer());
+    }
+
+    @Test
+    void testGiveDiceWhen6Or1IsRolled() {
+        OpusGamePanel testPanel = new OpusGamePanel("test");
+        Player p1 = new Player("p1");
+        Player p2 = new Player("p2");
+        testPanel.addPlayer(p1);
+        testPanel.addPlayer(p2);
+        testPanel.setCurrentPlayer(p1);
+        testPanel.giveDice(p2, 6, 1);
+        assertSame(p2, testPanel.getCurrentPlayer());
+    }
+
+    @Test
+    void displayDrinkMessageAfter3Minutes45Seconds(){
+        OpusGamePanel testPanel = new OpusGamePanel("test");
+        testPanel.startGame();
 
     }
+
 }
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.hydrohomies {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.hydrohomies to javafx.fxml;
