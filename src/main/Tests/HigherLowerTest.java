0c6e67c2d26f731de55ac552ba48f0c52f5e9765
josefjakobson <j.jakobson28@gmail.com>
2022-10-06 12:25:18 +0200

    Reshaped HigherLowerTest as well as starting work on HigherLowerController

--- src/main/Tests/HigherLowerTest.java
+++ src/main/Tests/HigherLowerTest.java
@@ -7,35 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HigherLowerTest {
-    @Test
-    void testAddPlayer(){
-        HigherLowerGame testGame = new HigherLowerGame();
-        Player player1 = new Player("player1");
-        testGame.getPlayers().addPlayer(player1);
-    }
 
-    @Test
-    void testSetCurrentPlayerAndGetCurrentPlayer(){
-        HigherLowerGame testGame = new HigherLowerGame();
-        Player player1 = new Player("player1");
-        testGame.setCurrentPlayer(player1);
-        assertTrue(testGame.getCurrentPlayer() == player1);
-    }
-    @Test
-    void testSetNextPlayer(){
-        HigherLowerGame testGame = new HigherLowerGame();
-        Player player1 = new Player("player1");
-        Player player2 = new Player("player2");
-        Player player3 = new Player("player3");
-        testGame.addPlayer(player1);
-        testGame.addPlayer(player2);
-
-        testGame.setCurrentPlayer(player1);
-        testGame.setNextPlayer();
-        assertTrue(testGame.getCurrentPlayer() == player2);
-        testGame.addPlayer(player3);
-        testGame.setNextPlayer();
-        assertTrue(testGame.getCurrentPlayer() == player3);
-    }
 
 }
