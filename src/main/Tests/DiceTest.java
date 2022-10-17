2a9a39d1785e897238724ff58438b1ab5c52589d
Fabian Forsman <fabian.forsman@gmail.com>
2022-10-17 22:42:11 +0200

    Changes by jjako

--- src/main/Tests/DiceTest.java
+++ src/main/Tests/DiceTest.java
@@ -1,2 +1,16 @@
+import Controllers.Treman.TremanController;
+import Model.DiceGames.Treman.TremanModel;
+import org.junit.jupiter.api.Test;
+
 public class DiceTest {
+
+    @Test
+    void testImage(){
+        TremanModel model = new TremanModel();
+        TremanController controller = new TremanController(model);
+        model.getDie(1).rollDie();
+        controller.setFirstDieImage();
+
+
+    }
 }
