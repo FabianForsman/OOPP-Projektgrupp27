40bad242e1ab4ccd5ee1065abfe5cb69d9d48f5a
Fabian Forsman <fabian.forsman@gmail.com>
2022-10-19 14:07:28 +0200

    Changes by Daniel

--- src/main/Tests/DiceTest.java
+++ src/main/Tests/DiceTest.java
@@ -8,6 +8,19 @@ import org.junit.jupiter.api.Test;
 
 public class DiceTest {
 
+    Player p1 = new Player("p1");
+    Player p2 = new Player("p2");
+    @BeforeEach
+    public void init() {
+        Players.getInstance().addPlayer(p1);
+        Players.getInstance().addPlayer(p2);
+    }
+
+    @AfterEach
+    public void teardown() {
+        Players.getInstance().getPlayersList().clear();
+    }
+
     @Test
     void testImage(){
         TremanModel model = new TremanModel();
