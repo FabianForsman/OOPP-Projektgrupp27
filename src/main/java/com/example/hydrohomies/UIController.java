4344ab5851ea4ac89fb62aaa586edb1cd0089fb6
Fabian Forsman <fabian.forsman@gmail.com>
2022-10-19 14:07:36 +0200

    Changes by julia

--- src/main/java/com/example/hydrohomies/UIController.java
+++ src/main/java/com/example/hydrohomies/UIController.java
@@ -1,6 +1,12 @@
 package com.example.hydrohomies;
 
+import Controllers.FTheDealer.FTheDealerController;
+import Controllers.HigherLower.HigherLowerController;
+import Controllers.Treman.TremanController;
+import Model.DiceGames.Treman.TremanModel;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -10,9 +16,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 
-public class UIController  {
+public class UIController implements Initializable {
 
-    @FXML public Button startCardGameButton, startDiceGameButton, startInformationButton, allItemsButton, exitButton;  //startPage
+    public TremanController tremanController = new TremanController(this);
+    // public HigherLowerController higherLowerController = new HigherLowerController(this);
+    // public FTheDealerController fTheDealerController = new FTheDealerController(this);
+    // public OpusController opusController = new OpusController(this);
+
+    @FXML public Button startCardGameButton, startDiceGameButton;  //startPage
     @FXML public Label  chooseGameLabel;
     @FXML public AnchorPane startGameAnchorPane, diceGameAnchorPane, cardGameAnchorpane, opusGameAnchorPane, tremanGameAnchorPane, higherLowerAnchorPane;
 
@@ -21,4 +32,10 @@ public class UIController  {
         tremanGameAnchorPane.toFront();
     }
 
+    public void roll(ActionEvent actionEvent) {
+        tremanController.roll(actionEvent);
+    }
+
+    @FXML
+    public void roll2(ActionEvent actionEvent) { tremanController.roll2(actionEvent);}
 }
